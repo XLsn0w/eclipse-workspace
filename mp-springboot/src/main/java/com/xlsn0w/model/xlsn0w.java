612e49c27a89d8a253015b481796be20b2838a72
xlsn0w <xlsn0w@outlook.com>
2017-11-30 10:31:57 +0800

    add

---
+++ mp-springboot/src/main/java/com/xlsn0w/model/xlsn0w.java
@@ -0,0 +1,14 @@
+package com.xlsn0w.model;
+
+public class xlsn0w {
+	private String model = "xlsn0w";
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+}
